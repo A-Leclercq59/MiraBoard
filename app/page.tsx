32f63dc35b59e397398b227cf4d4605ae526c3b0
Anthony Leclercq <aleclercq59.dev@gmail.com>
2024-02-15 15:28:58 +0100

    Convex & Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p>Hello world</p>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for autenticated users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
