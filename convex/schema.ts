29ee1c97debe93baa44b1eb675d57d6de061a057
Anthony Leclercq <aleclercq59.dev@gmail.com>
2024-02-20 22:10:56 +0100

    Favorite functionality

--- convex/schema.ts
+++ convex/schema.ts
@@ -14,4 +14,13 @@ export default defineSchema({
       searchField: "title",
       filterFields: ["orgId"],
     }),
+  userFavorites: defineTable({
+    ordId: v.string(),
+    userId: v.string(),
+    boardId: v.id("boards"),
+  })
+    .index("by_board", ["boardId"])
+    .index("by_user_org", ["userId", "ordId"])
+    .index("by_user_board", ["userId", "boardId"])
+    .index("by_user_board_org", ["userId", "boardId", "ordId"]),
 });
